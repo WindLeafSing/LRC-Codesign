5d359ff980abd4993e1b07e0fe486d2f84efd558
杜清鹏 <1361229831@qq.com>
2020-12-27 18:22:36 +0800

    layout

--- include/Common.h
+++ include/Common.h
@@ -7,16 +7,13 @@
 
 void shuffle_helper(std::vector<int> & comb)
 {
-    std::default_random_engine dre(42);
+    std::default_random_engine dre(rand());
     std::uniform_int_distribution<int> uid(0,comb.size()-1);
-    int shuffle_times = 100*uid(dre);
-    while(shuffle_times--)
-    {
         int from = uid(dre);
         int to = uid(dre) ;
         while(from==to) to=uid(dre);
         std::swap(comb[from],comb[to]);
-    }
+
     
 }
 
